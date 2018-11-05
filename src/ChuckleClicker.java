da3a1eab0738314b54005cea24492993874083b0
league <league@imac-6.attlocal.net>
2018-11-04 16:00:44 -0800

    TypingTutor

--- src/ChuckleClicker.java
+++ src/ChuckleClicker.java
@@ -48,3 +48,4 @@ public class ChuckleClicker implements ActionListener {
 
 	}
 }
+	
--- src/TypingTutor.java
+++ src/TypingTutor.java
@@ -1,8 +1,12 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Random;
 
+import javax.swing.AbstractButton;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -11,21 +15,52 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class TypingTutor implements ActionListener {
+import sun.launcher.resources.launcher;
+
+public class TypingTutor implements KeyListener {
 	public static void main(String[] args) throws Exception {
 		TypingTutor typingtutor = new TypingTutor();
-		typingtutor.makeButtons();
-	}
-	JFrame j = new JFrame();
-	private void makeButtons() {
-		j.setVisible(true);
-		j.setDefaultCloseOperation(j.EXIT_ON_CLOSE);
-		j.setSize(500, 500);
-
-
-	}
-	@Override
-	public void actionPerformed(ActionEvent e) {
+		typingtutor.currentLetter();
+		
+}
+JFrame j = new JFrame();
+JLabel l = new JLabel();
+char currentLetter;
+private void currentLetter() {
 		// TODO Auto-generated method stub
-	}
-	}
+	currentLetter = generateRandomLetter();
+	j.setVisible(true);
+	j.setDefaultCloseOperation(j.EXIT_ON_CLOSE);
+	j.setSize(500, 500);
+	j.add(l);
+	l.setText(""+currentLetter);
+	l.setFont(j.getFont().deriveFont(28.0f));
+    l.setHorizontalAlignment(JLabel.CENTER);
+    j.addKeyListener(this);
+}
+
+char generateRandomLetter() {
+	Random r = new Random();
+	return (char) (r.nextInt(26) + 'a');
+}
+@Override
+public void keyTyped(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+@Override
+public void keyPressed(KeyEvent e) {
+	// TODO Auto-generated method stub
+	System.out.println(currentLetter);
+	
+}
+@Override
+public void keyReleased(KeyEvent e) {
+	// TODO Auto-generated method stub
+	currentLetter = generateRandomLetter();
+	l.setText(""+currentLetter);
+}
+}
+
+
+	
