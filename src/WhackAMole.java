d8e36ecc4a6f6d42aaad200047a8b05fb146ec63
league <league@wts14-2.attlocal.net>
2018-12-02 15:57:00 -0800

    whackamole

---
+++ src/WhackAMole.java
@@ -0,0 +1,107 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Date;
+import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class WhackAMole implements ActionListener {
+	public static void main(String[] args) throws Exception {
+		WhackAMole whackamole = new WhackAMole();
+		whackamole.mole();
+		Random generator = new Random();
+		whackamole.drawButtons(generator.nextInt(6));
+}
+	
+	JFrame f = new JFrame();
+	JButton b1 = new JButton();
+	JButton b2 = new JButton();
+	JButton b3 = new JButton();
+	JButton b4 = new JButton();
+	JButton b5 = new JButton();
+	JButton b6 = new JButton();
+	JPanel p = new JPanel();
+	private void mole() {
+		f.setVisible(true);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setSize(150, 250);
+		f.add(p);
+		p.add(b1);
+		p.add(b2);
+		p.add(b3);
+		p.add(b4);
+		p.add(b5);
+		p.add(b6);
+		b1.addActionListener(this);
+		b2.addActionListener(this);
+		b3.addActionListener(this);
+		b4.addActionListener(this);
+		b5.addActionListener(this);
+		b6.addActionListener(this);
+	}
+	private void drawButtons(int number) {
+		if (number == 0) {
+			b1.setText("mole!");
+		}
+		if (number == 1) {
+			b2.setText("mole!");
+		}
+		if (number == 2) {
+			b3.setText("mole!");
+		}
+		if (number == 3) {
+			b4.setText("mole!");
+		}
+		if (number == 4) {
+			b5.setText("mole!");
+		}
+		if (number == 5) {
+			b6.setText("mole!");
+		}
+	}
+	public void reset() {
+		b1.setText(null);
+		b2.setText(null);
+		b3.setText(null);
+		b4.setText(null);
+		b5.setText(null);
+		b6.setText(null);
+		Random generator = new Random();
+		drawButtons(generator.nextInt(6));
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource() == b1 && b1.getText() != "mole!") {
+			System.out.println("you missed!");
+			f.dispose();
+		}
+		if (e.getSource() == b2 && b2.getText() != "mole!") {
+			System.out.println("you missed!");
+			f.dispose();
+		}
+		if (e.getSource() == b3 && b3.getText() != "mole!") {
+			System.out.println("you missed!");
+			f.dispose();
+		}
+		if (e.getSource() == b4 && b4.getText() != "mole!") {
+			System.out.println("you missed!");
+			f.dispose();
+		}
+		if (e.getSource() == b5 && b5.getText() != "mole!") {
+			System.out.println("you missed!");
+			f.dispose();
+		}
+		if (e.getSource() == b6 && b6.getText() != "mole!") {
+			System.out.println("you missed!");
+			f.dispose();
+		}
+		reset();
+
+	}
+	
+}
